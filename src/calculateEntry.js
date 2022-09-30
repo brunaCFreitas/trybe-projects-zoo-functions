439f007229c028a32971c559a79c087a0201786f
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-09-30 16:17:04 -0300

    Adiciona requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui vamo q vamo
+  const childrens = entrants.filter((entrant) => entrant.age < 18);
+  const grownUp = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
+  const old = entrants.filter((entrant) => entrant.age >= 50);
+  return {
+    child: childrens.length,
+    adult: grownUp.length,
+    senior: old.length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+
+  const objentrants = countEntrants(entrants);
+  const { child, adult, senior } = objentrants;
+  const value = (child * prices.child) + (adult * prices.adult) + (senior * prices.senior);
+  return value;
 }
+
+console.log(calculateEntry({}));
 
 module.exports = { calculateEntry, countEntrants };
