613e3d77679bee9986f12b4988cb94d353a1f0d8
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-09-30 15:21:49 -0300

    Adiciona requsito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
+
+const countAnimals = (animal) => {
+  const obj = {};
+  if (!animal) {
+    species.forEach(({ name, residents }) => {
+      obj[name] = residents.length;
+    });
+    return obj;
+  }
+
+  const animalFind = species.find(({ name }) => name === animal.specie);
+
+  if (animal.specie && animal.sex) {
+    return animalFind.residents.filter(({ sex }) => sex === animal.sex).length;
+  }
+  return animalFind.residents.length;
+};
 
 module.exports = countAnimals;
