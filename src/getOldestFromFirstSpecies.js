beddac6e9620f56245a227d68bbf0edfe773326d
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-10-03 15:05:16 -0300

    Adiciona requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeId = employees.find((employee) => employee.id === id);
+  const specieFirst = employeeId.responsibleFor[0];
+  const listResidents = species.find((specie) => specie.id === specieFirst).residents;
+  const oldestAnimal = listResidents.reduce((prev, curr) => ((prev.age > curr.age) ? prev : curr));
+  const array = [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
+  return array;
 }
+
+console.log(getOldestFromFirstSpecies('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 
 module.exports = getOldestFromFirstSpecies;
