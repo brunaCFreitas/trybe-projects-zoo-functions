25f9776ccbc9200f2c9fc82e0f9d9893cd857f09
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-10-03 18:08:42 -0300

    Adiciona requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,54 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const { employees, species } = data;
+
+const getSpeciesByIds = (ids) => {
+  const animals = species.filter((specie) => ids.includes(specie.id));
+  return animals.map((animal) => animal.name);
+};
+
+const getLocationByIds = (ids) => {
+  const locationAnimals = species.filter((specie) => ids.includes(specie.id));
+  return locationAnimals.map((locationAnimal) => locationAnimal.location);
+};
+
+const objReturn = (employeeSel) => ({
+  id: employeeSel.id,
+  fullName: `${employeeSel.firstName} ${employeeSel.lastName}`,
+  species: getSpeciesByIds(employeeSel.responsibleFor),
+  locations: getLocationByIds(employeeSel.responsibleFor),
+});
+
+const getEmployeesByname = (obj) => {
+  const { name } = obj;
+  const employeeSel = employees
+    .find(({ firstName, lastName }) => firstName === name || lastName === name);
+  if (!employeeSel) {
+    throw new Error('Informações inválidas');
+  }
+  return objReturn(employeeSel);
+};
+
+const getEmployeesById = (obj) => {
+  const employeeSel = employees.find((employee) => employee.id === obj.id);
+  if (!employeeSel) {
+    throw new Error('Informações inválidas');
+  }
+  return objReturn(employeeSel);
+};
+
+const getEmployeesCoverage = (obj) => {
+  if (obj === undefined) {
+    return employees.map((funcionario) => objReturn(funcionario));
+  }
+
+  if (obj.name) {
+    return getEmployeesByname(obj);
+  }
+
+  if (obj.id) {
+    return getEmployeesById(obj);
+  }
+};
 
 module.exports = getEmployeesCoverage;
