816cb0fdf38abdbd2e42990d11f3a35e86f538a0
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-10-03 20:18:58 -0300

    Refatora requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,15 +1,13 @@
 const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  const employeeId = employees.find((employee) => employee.id === id);
-  const specieFirst = employeeId.responsibleFor[0];
-  const listResidents = species.find((specie) => specie.id === specieFirst).residents;
-  const oldestAnimal = listResidents.reduce((prev, curr) => ((prev.age > curr.age) ? prev : curr));
-  const array = [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
-  return array;
+function getOldestFromFirstSpecies(employeeId) {
+  const employee = employees.find(({ id }) => id === employeeId);
+  const [specieFirstId] = employee.responsibleFor;
+  const { residents } = species.find((specie) => specie.id === specieFirstId);
+  const { name, sex, age } = residents
+    .reduce((prev, curr) => ((prev.age > curr.age) ? prev : curr));
+  return [name, sex, age];
 }
-
-console.log(getOldestFromFirstSpecies('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 
 module.exports = getOldestFromFirstSpecies;
